56c4eee58f9bf84b3b33fe5ea25a07e59381bf35
kir-san <provakatory@gmail.com>
2024-05-26 12:07:47 +0300

    update strings.xml

--- features/settings/src/main/kotlin/com/san/kir/settings/ui/settings/SettingsScreen.kt
+++ features/settings/src/main/kotlin/com/san/kir/settings/ui/settings/SettingsScreen.kt
@@ -71,8 +71,8 @@ internal fun SettingsScreen(
 @Composable
 private fun Main(main: Settings.Main, sendAction: (SettingsAction.SaveMain) -> Unit) {
     TogglePreferenceItem(
-        title = R.string.settings_app_dark_theme_title,
-        subtitle = R.string.settings_app_dark_theme_summary,
+        title = R.string.dark_theme_title,
+        subtitle = R.string.dark_theme_summary,
         icon = Icons.Default.DarkMode,
         initialValue = main.theme,
         onCheckedChange = { sendAction(SettingsAction.SaveMain(main.copy(theme = it))) }
@@ -80,11 +80,11 @@ private fun Main(main: Settings.Main, sendAction: (SettingsAction.SaveMain) -> U
 
     HorizontalDivider()
 
-    PreferenceTitle(R.string.settings_library_title)
+    PreferenceTitle(R.string.library_title)
 
     TogglePreferenceItem(
-        title = R.string.settings_library_show_category_title,
-        subtitle = R.string.settings_library_show_category_summary,
+        title = R.string.show_category_title,
+        subtitle = R.string.show_category_summary,
         icon = Icons.Default.Category,
         initialValue = main.isShowCategory,
         onCheckedChange = { sendAction(SettingsAction.SaveMain(main.copy(isShowCategory = it))) }
@@ -93,19 +93,19 @@ private fun Main(main: Settings.Main, sendAction: (SettingsAction.SaveMain) -> U
 
 @Composable
 private fun Chapters(chapters: Settings.Chapters, sendAction: (SettingsAction.SaveChapters) -> Unit) {
-    PreferenceTitle(R.string.settings_list_chapter_title)
+    PreferenceTitle(R.string.list_chapter_title)
 
     TogglePreferenceItem(
-        title = R.string.settings_list_chapter_filter_title,
-        subtitle = R.string.settings_list_chapter_filter_summary,
+        title = R.string.list_chapter_filter_title,
+        subtitle = R.string.list_chapter_filter_summary,
         icon = Icons.Default.FilterList,
         initialValue = chapters.isIndividual,
         onCheckedChange = { sendAction(SettingsAction.SaveChapters(chapters.copy(isIndividual = it))) }
     )
 
     TogglePreferenceItem(
-        title = R.string.settings_list_chapter_title_title,
-        subtitle = R.string.settings_list_chapter_title_summary,
+        title = R.string.list_chapter_title_title,
+        subtitle = R.string.list_chapter_title_summary,
         icon = Icons.Default.Title,
         initialValue = chapters.isTitle,
         onCheckedChange = { sendAction(SettingsAction.SaveChapters(chapters.copy(isTitle = it))) }
@@ -114,23 +114,23 @@ private fun Chapters(chapters: Settings.Chapters, sendAction: (SettingsAction.Sa
 
 @Composable
 private fun Viewer(viewer: Settings.Viewer, sendAction: (SettingsAction.SaveViewer) -> Unit) {
-    PreferenceTitle(R.string.settings_viewer_title)
+    PreferenceTitle(R.string.viewer_title)
 
     ListPreferenceItem(
-        title = R.string.settings_viewer_orientation_title,
-        subtitle = R.string.settings_viewer_orientation_summary,
+        title = R.string.viewer_orientation_title,
+        subtitle = R.string.viewer_orientation_summary,
         icon = Icons.Default.CropLandscape,
-        entries = R.array.settings_viewer_orientation_array,
+        entries = R.array.viewer_orientation_array,
         entryValues = Orientation.entries,
         initialValue = viewer.orientation,
         onValueChange = { sendAction(SettingsAction.SaveViewer(viewer.copy(orientation = it))) }
     )
 
     MultiSelectListPreferenceItem(
-        title = R.string.settings_viewer_control_title,
-        subtitle = R.string.settings_viewer_control_summary,
+        title = R.string.viewer_control_title,
+        subtitle = R.string.viewer_control_summary,
         icon = Icons.Default.VideogameAsset,
-        entries = R.array.settings_viewer_control_array,
+        entries = R.array.viewer_control_array,
         initialValue = viewer.controls,
         onValueChange = {
             sendAction(SettingsAction.SaveViewer(viewer.copy(control = viewer.controls(it))))
@@ -139,24 +139,24 @@ private fun Viewer(viewer: Settings.Viewer, sendAction: (SettingsAction.SaveView
 
     if (Build.VERSION.SDK_INT >= 28)
         TogglePreferenceItem(
-            title = R.string.settings_viewer_cutout_title,
-            subtitle = R.string.settings_viewer_cutout_summary,
+            title = R.string.viewer_cutout_title,
+            subtitle = R.string.viewer_cutout_summary,
             icon = Icons.Default.ContentCut,
             initialValue = viewer.cutOut,
             onCheckedChange = { sendAction(SettingsAction.SaveViewer(viewer.copy(cutOut = it))) }
         )
 
     TogglePreferenceItem(
-        title = R.string.settings_viewer_without_title,
-        subtitle = R.string.settings_viewer_without_summary,
+        title = R.string.viewer_without_title,
+        subtitle = R.string.viewer_without_summary,
         //            icon = Icons.Default.ContentCut,
         initialValue = viewer.withoutSaveFiles,
         onCheckedChange = { sendAction(SettingsAction.SaveViewer(viewer.copy(withoutSaveFiles = it))) }
     )
 
     TogglePreferenceItem(
-        title = R.string.settings_viewer_scrollbars_title,
-        subtitle = R.string.settings_viewer_scrollbars_summary,
+        title = R.string.viewer_scrollbars_title,
+        subtitle = R.string.viewer_scrollbars_summary,
         icon = Icons.Default.FitScreen,
         initialValue = viewer.useScrollbars,
         onCheckedChange = { sendAction(SettingsAction.SaveViewer(viewer.copy(useScrollbars = it))) }
@@ -166,27 +166,27 @@ private fun Viewer(viewer: Settings.Viewer, sendAction: (SettingsAction.SaveView
 
 @Composable
 private fun Download(download: Settings.Download, sendAction: (SettingsAction.SaveDownload) -> Unit) {
-    PreferenceTitle(R.string.settings_downloader_title)
+    PreferenceTitle(R.string.downloader_title)
 
     TogglePreferenceItem(
-        title = R.string.settings_downloader_parallel_title,
-        subtitle = R.string.settings_downloader_parallel_summary,
+        title = R.string.downloader_parallel_title,
+        subtitle = R.string.downloader_parallel_summary,
         icon = Icons.AutoMirrored.Filled.CompareArrows,
         initialValue = download.concurrent,
         onCheckedChange = { sendAction(SettingsAction.SaveDownload(download.copy(concurrent = it))) }
     )
 
     TogglePreferenceItem(
-        title = R.string.settings_downloader_retry_title,
-        subtitle = R.string.settings_downloader_retry_summary,
+        title = R.string.downloader_retry_title,
+        subtitle = R.string.downloader_retry_summary,
         icon = Icons.Default.ErrorOutline,
         initialValue = download.retry,
         onCheckedChange = { sendAction(SettingsAction.SaveDownload(download.copy(retry = it))) }
     )
 
     TogglePreferenceItem(
-        title = R.string.settings_downloader_wifi_only_title,
-        subtitle = R.string.settings_downloader_wifi_only_summary,
+        title = R.string.downloader_wifi_only_title,
+        subtitle = R.string.downloader_wifi_only_summary,
         icon = Icons.Default.Wifi,
         initialValue = download.wifi,
         onCheckedChange = { sendAction(SettingsAction.SaveDownload(download.copy(wifi = it))) }
