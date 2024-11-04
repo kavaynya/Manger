1789f779ebb61e3da994fb8995d4f822ceb94254
kir-san <provakatory@gmail.com>
2024-11-04 15:29:17 +0300

    add new methods to retrieve data. fix the insert method. update the save method to merge old and new data.

--- data/db/src/main/kotlin/com/san/kir/data/db/catalog/dao/SiteCatalogDao.kt
+++ data/db/src/main/kotlin/com/san/kir/data/db/catalog/dao/SiteCatalogDao.kt
@@ -5,17 +5,25 @@ import androidx.room.Query
 import com.san.kir.data.db.base.BaseDao
 import com.san.kir.data.db.catalog.custom.DbSimplifiedCatalogItem
 import com.san.kir.data.db.catalog.entities.DbSiteCatalogElement
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 internal interface SiteCatalogDao : BaseDao<DbSiteCatalogElement> {
-    @Query("SELECT * FROM items")
-    suspend fun items(): List<DbSiteCatalogElement>
+
+    @Query("SELECT * FROM items LIMIT :limit OFFSET :offset")
+    suspend fun items(limit: Int = 500, offset: Int = 0): List<DbSiteCatalogElement>
+
+    @Query("SELECT id, catalogName, name, statusEdition, shotLink, link, logo, genres, type, authors, dateId, populate FROM items")
+    fun loadMinitItems(): Flow<List<DbSimplifiedCatalogItem>>
 
     @Query("SELECT id, catalogName, name, statusEdition, shotLink, link, logo, genres, type, authors, dateId, populate FROM items")
     suspend fun miniItems(): List<DbSimplifiedCatalogItem>
 
     @Query("SELECT * FROM items WHERE id=:id")
     suspend fun itemById(id: Long): DbSiteCatalogElement?
+
+    @Query("SELECT * FROM items WHERE link=:link")
+    suspend fun itemByLink(link: String): DbSiteCatalogElement?
 
     @Query("SELECT COUNT(id) FROM items")
     suspend fun itemsCount(): Int
--- data/db/src/main/kotlin/com/san/kir/data/db/catalog/repo/CatalogsRepository.kt
+++ data/db/src/main/kotlin/com/san/kir/data/db/catalog/repo/CatalogsRepository.kt
@@ -9,8 +9,13 @@ import com.san.kir.data.db.catalog.mappers.toModel
 import com.san.kir.data.db.catalog.mappers.toModels
 import com.san.kir.data.models.catalog.MiniCatalogItem
 import com.san.kir.data.models.catalog.SiteCatalogElement
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 public class CatalogsRepository internal constructor(private val catalogFactory: (String) -> CatalogDb) {
+
+    public fun loadItems(catalogName: String): Flow<List<MiniCatalogItem>> =
+        catalogFactory(catalogName).dao.loadMinitItems().map { it.toModels() }
 
     public suspend fun items(catalogName: String): List<MiniCatalogItem> =
         use(catalogName) { miniItems().toModels() }
@@ -20,23 +25,59 @@ public class CatalogsRepository internal constructor(private val catalogFactory:
 
     public suspend fun volume(catalogName: String): Int = use(catalogName) { itemsCount() }
 
-    public suspend fun save(catalogName: String, list: List<SiteCatalogElement>): List<Long> =
+    public suspend fun save(catalogName: String, list: List<SiteCatalogElement>): Unit =
         use(catalogName) {
-            deleteAll()
+            var limit = 500
+            var offset = 0
 
             val unics = mutableListOf<String>()
-            val entities = list.filter { item ->
-                val notContain = unics.contains(item.link).not()
-                if (notContain) unics.add(item.link)
-                notContain
-            }.toEntities()
+
+            val entities = list
+                .filter { item ->
+                    val notContain = unics.contains(item.link).not()
+                    if (notContain) unics.add(item.link)
+                    notContain
+                }
+                .toEntities()
+                .toMutableList()
+
+            do {
+                val items = items(limit, offset).map {
+                    var dbItem = it
+
+                    entities.firstOrNull { it.link == dbItem.link }?.let { newItem ->
+                        dbItem = dbItem.copy(
+                            name = newItem.name,
+                            logo = newItem.logo.ifBlank { dbItem.logo },
+                            dateId = if (newItem.dateId == 0) dbItem.dateId else newItem.dateId,
+                            populate = if (newItem.populate == 0) dbItem.populate else newItem.populate,
+                            about = newItem.about.ifBlank { dbItem.about },
+                            volume = maxOf(newItem.volume, dbItem.volume),
+                            statusEdition = newItem.statusEdition.ifBlank { dbItem.statusEdition },
+                            statusTranslate = newItem.statusTranslate.ifBlank { dbItem.statusTranslate },
+                            authors = dbItem.authors.union(newItem.authors).toList(),
+                            genres = dbItem.genres.union(newItem.genres).toList(),
+                        )
+                        entities.remove(newItem)
+                    }
+
+                    dbItem
+                }
+                insert(items)
+                offset += limit
+            } while (items.isNotEmpty())
 
             insert(entities)
         }
 
     public suspend fun insert(catalogName: String, item: SiteCatalogElement): List<Long> =
         use(catalogName) {
-            insert(item.toEntity())
+            val id = if (item.id > 0) {
+                item.id
+            } else {
+                itemByLink(item.link)?.id ?: 0
+            }
+            insert(item.toEntity().copy(id = id))
         }
 
     private suspend fun <R> use(catalogName: String, action: suspend SiteCatalogDao.() -> R) =
