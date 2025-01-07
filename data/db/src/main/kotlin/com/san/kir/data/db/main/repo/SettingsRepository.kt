178a0d9d0666c2415f847d8be2d247cbb5abbfd6
kir-san <provakatory@gmail.com>
2025-01-07 14:32:40 +0300

    Remove coroutine context switch Add distinctUntilChanged

--- data/db/src/main/kotlin/com/san/kir/data/db/main/repo/SettingsRepository.kt
+++ data/db/src/main/kotlin/com/san/kir/data/db/main/repo/SettingsRepository.kt
@@ -1,11 +1,11 @@
 package com.san.kir.data.db.main.repo
 
-import com.san.kir.core.utils.coroutines.withIoContext
 import com.san.kir.data.db.main.dao.SettingsDao
 import com.san.kir.data.db.main.mappers.toModel
 import com.san.kir.data.models.main.Settings
 import com.san.kir.data.models.utils.ChapterFilter
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
@@ -13,15 +13,15 @@ import kotlinx.coroutines.flow.map
 
 public class SettingsRepository internal constructor(private val settingsDao: SettingsDao) {
     private val _settings = settingsDao.loadItem().filterNotNull()
-    public val settings: Flow<Settings> = _settings.toModel()
-    public val isDarkTheme: Flow<Boolean> = _settings.map { it.isDarkTheme }
-    public val wifi: Flow<Boolean> = _settings.map { it.wifi }
-    public val showTitle: Flow<Boolean> = _settings.map { it.isTitle }
-    public val isIndividual: Flow<Boolean> = _settings.map { it.isIndividual }
-    public val isShowCategory: Flow<Boolean> = _settings.map { it.isShowCategory }
-    public val useScrollbars: Flow<Boolean> = _settings.map { it.useScrollbars }
-    public val viewer: Flow<Settings.Viewer> = settings.map { it.viewer }
-    public val control: Flow<Settings.Viewer.Control> = viewer.map { it.control }
+    public val settings: Flow<Settings> = _settings.toModel().distinctUntilChanged()
+    public val isDarkTheme: Flow<Boolean> = _settings.map { it.isDarkTheme }.distinctUntilChanged()
+    public val wifi: Flow<Boolean> = _settings.map { it.wifi }.distinctUntilChanged()
+    public val showTitle: Flow<Boolean> = _settings.map { it.isTitle }.distinctUntilChanged()
+    public val isIndividual: Flow<Boolean> = _settings.map { it.isIndividual }.distinctUntilChanged()
+    public val isShowCategory: Flow<Boolean> = _settings.map { it.isShowCategory }.distinctUntilChanged()
+    public val useScrollbars: Flow<Boolean> = _settings.map { it.useScrollbars }.distinctUntilChanged()
+    public val viewer: Flow<Settings.Viewer> = settings.map { it.viewer }.distinctUntilChanged()
+    public val control: Flow<Settings.Viewer.Control> = viewer.map { it.control }.distinctUntilChanged()
 
     private suspend fun currentSettings() = _settings.first()
     public suspend fun isIndividual(): Boolean = currentSettings().isIndividual
@@ -32,49 +32,41 @@ public class SettingsRepository internal constructor(private val settingsDao: Se
     public suspend fun wifi(): Boolean = currentSettings().wifi
 
     public suspend fun update(newFilter: ChapterFilter): Unit =
-        withIoContext { settingsDao.update(currentSettings().copy(filterStatus = newFilter)) }
+        settingsDao.update(currentSettings().copy(filterStatus = newFilter))
 
-    public suspend fun update(state: Settings.Chapters): Unit = withIoContext {
-        settingsDao.update(
-            currentSettings().copy(
-                isIndividual = state.isIndividual,
-                isTitle = state.isTitle,
-                filterStatus = state.filterStatus
-            )
+    public suspend fun update(state: Settings.Chapters): Unit = settingsDao.update(
+        currentSettings().copy(
+            isIndividual = state.isIndividual,
+            isTitle = state.isTitle,
+            filterStatus = state.filterStatus
         )
-    }
+    )
 
-    public suspend fun update(state: Settings.Download): Unit = withIoContext {
-        settingsDao.update(
-            currentSettings().copy(
-                concurrent = state.concurrent,
-                retry = state.retry,
-                wifi = state.wifi
-            )
+    public suspend fun update(state: Settings.Download): Unit = settingsDao.update(
+        currentSettings().copy(
+            concurrent = state.concurrent,
+            retry = state.retry,
+            wifi = state.wifi
         )
-    }
+    )
 
-    public suspend fun update(state: Settings.Main): Unit = withIoContext {
-        settingsDao.update(
-            currentSettings().copy(
-                isDarkTheme = state.isDarkTheme,
-                isShowCategory = state.isShowCategory,
-                editMenu = state.editMenu
-            )
+    public suspend fun update(state: Settings.Main): Unit = settingsDao.update(
+        currentSettings().copy(
+            isDarkTheme = state.isDarkTheme,
+            isShowCategory = state.isShowCategory,
+            editMenu = state.editMenu
         )
-    }
+    )
 
-    public suspend fun update(state: Settings.Viewer): Unit = withIoContext {
-        settingsDao.update(
-            currentSettings().copy(
-                orientation = state.orientation,
-                cutOut = state.cutOut,
-                withoutSaveFiles = state.withoutSaveFiles,
-                useScrollbars = state.useScrollbars,
-                taps = state.control.taps,
-                swipes = state.control.swipes,
-                keys = state.control.keys,
-            )
+    public suspend fun update(state: Settings.Viewer): Unit = settingsDao.update(
+        currentSettings().copy(
+            orientation = state.orientation,
+            cutOut = state.cutOut,
+            withoutSaveFiles = state.withoutSaveFiles,
+            useScrollbars = state.useScrollbars,
+            taps = state.control.taps,
+            swipes = state.control.swipes,
+            keys = state.control.keys,
         )
-    }
+    )
 }
